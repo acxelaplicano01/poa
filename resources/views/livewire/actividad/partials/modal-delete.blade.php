7fa05f31cc6955a8a33f4bdf3fee35804dd073ce
acxel.aplicano <acxel.aplicano@unah.hn>
2025-11-21 20:30:06 -0600

    actividades corregidas al eliminar

--- resources/views/livewire/actividad/partials/modal-delete.blade.php
+++ resources/views/livewire/actividad/partials/modal-delete.blade.php
@@ -1,5 +1,5 @@
 {{-- Modal Confirmar Eliminación --}}
-<x-confirmation-modal wire:model="modalDelete">
+<x-confirmation-modal wire:model="modalDelete" maxWidth="lg">
     <x-slot name="title">
         Eliminar Actividad
     </x-slot>
@@ -11,10 +11,10 @@
             </p>
             
             <div class="mt-4 bg-zinc-50 dark:bg-zinc-800/50 rounded-lg p-4 space-y-2 text-sm">
-                @if($actividadToDelete->tipoActividad)
+                @if($actividadToDelete->tipo)
                     <div>
                         <span class="font-medium text-zinc-700 dark:text-zinc-300">Tipo:</span>
-                        <span class="text-zinc-600 dark:text-zinc-400">{{ $actividadToDelete->tipoActividad->tipo }}</span>
+                        <span class="text-zinc-600 dark:text-zinc-400">{{ $actividadToDelete->tipo->tipo }}</span>
                     </div>
                 @endif
                 
@@ -26,19 +26,30 @@
                 @endif
             </div>
 
-            <p class="text-sm text-red-600 dark:text-red-400 mt-4">
-                Esta acción no se puede deshacer.
-            </p>
+            <div class="mt-4 p-3 bg-yellow-50 dark:bg-yellow-900 border border-yellow-200 dark:border-yellow-700 rounded-md">
+                <div class="flex">
+                    <div class="flex-shrink-0">
+                        <svg class="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <div class="ml-3">
+                        <p class="text-sm text-yellow-700 dark:text-yellow-200">
+                            {{ __('Esta acción no se puede deshacer. Se eliminarán permanentemente todos los datos asociados.') }}
+                        </p>
+                    </div>
+                </div>
+            </div>
         @endif
     </x-slot>
 
     <x-slot name="footer">
-        <x-secondary-button wire:click="$set('modalDelete', false)">
+        <x-spinner-secondary-button wire:click="$set('modalDelete', false)" loadingTarget="$set('modalDelete', false)" loadingText="Cerrando...">
             Cancelar
-        </x-secondary-button>
+        </x-spinner-secondary-button>
 
-        <x-danger-button wire:click="eliminar" class="ml-3">
+        <x-spinner-danger-button wire:click="eliminar" loadingTarget="eliminar" loadingText="Eliminando..." class="ml-3">
             Eliminar Actividad
-        </x-danger-button>
+        </x-spinner-danger-button>
     </x-slot>
 </x-confirmation-modal>
