3a293fe1c8a61eadaf9d70acb5644436ddd26679
acxel.aplicano <acxel.aplicano@unah.hn>
2025-12-21 23:10:01 -0600

    Historial de revisiones ajustado

--- resources/views/livewire/actividad/gestionar-actividad.blade.php
+++ resources/views/livewire/actividad/gestionar-actividad.blade.php
@@ -79,7 +79,7 @@
                         @php
                             $todosComentarios = $actividad->revisiones()
                                 ->with('user')
-                                ->whereIn('tipo', ['TAREA', 'INDICADOR', 'PLANIFICACION'])
+                                ->whereIn('tipo', ['TAREA', 'INDICADOR', 'PLANIFICACION', 'REVISION', 'DICTAMEN'])
                                 ->orderBy('created_at', 'desc')
                                 ->get();
                         @endphp
@@ -128,6 +128,18 @@
                                             $colorFondo = 'bg-indigo-100 dark:bg-indigo-900/30';
                                             $colorTexto = 'text-indigo-800 dark:text-indigo-300';
                                             break;
+                                        case 'REVISION':
+                                            $nombreElemento = 'Mensaje de Revisión';
+                                            $tipoElemento = 'Revisión';
+                                            $colorFondo = 'bg-orange-100 dark:bg-orange-900/30';
+                                            $colorTexto = 'text-orange-800 dark:text-orange-300';
+                                            break;
+                                        case 'DICTAMEN':
+                                            $nombreElemento = 'Dictamen Final';
+                                            $tipoElemento = 'Dictamen';
+                                            $colorFondo = 'bg-red-100 dark:bg-red-900/30';
+                                            $colorTexto = 'text-red-800 dark:text-red-300';
+                                            break;
                                     }
                                 @endphp
                                 
@@ -156,23 +168,43 @@
                                         {{ $comentario->revision }}
                                     </p>
                                     
-                                    <div class="flex items-center justify-between">
-                                        @if($comentario->corregido)
-                                            <span class="inline-flex items-center px-2.5 py-1 bg-green-100 dark:bg-green-900/30 text-green-800 dark:text-green-300 text-xs font-semibold rounded-md">
-                                                <svg class="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20">
-                                                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
-                                                </svg>
-                                                Corregido
-                                            </span>
-                                        @else
-                                            <span class="inline-flex items-center px-2.5 py-1 bg-orange-100 dark:bg-orange-900/30 text-orange-800 dark:text-orange-300 text-xs font-semibold rounded-md">
-                                                <svg class="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20">
-                                                    <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"/>
-                                                </svg>
-                                                Pendiente
-                                            </span>
-                                        @endif
-                                    </div>
+                                    @if($comentario->tipo === 'DICTAMEN')
+                                        <div class="flex items-center justify-between">
+                                            @if($actividad->estado === 'APROBADO')
+                                                <span class="inline-flex items-center px-2.5 py-1 bg-green-100 dark:bg-green-900/30 text-green-800 dark:text-green-300 text-xs font-semibold rounded-md">
+                                                    <svg class="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20">
+                                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
+                                                    </svg>
+                                                    Actividad Aprobada
+                                                </span>
+                                            @elseif($actividad->estado === 'RECHAZADO')
+                                                <span class="inline-flex items-center px-2.5 py-1 bg-red-100 dark:bg-red-900/30 text-red-800 dark:text-red-300 text-xs font-semibold rounded-md">
+                                                    <svg class="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20">
+                                                        <path fill-rule="evenodd" d="M10 18a8 8 0 11-16 0 8 8 0 0116 0zm-8.707-5.293a1 1 0 011.414 0L10 15.293l5.293-5.293a1 1 0 111.414 1.414l-6 6a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414z" clip-rule="evenodd"/>
+                                                    </svg>
+                                                    Actividad Rechazada
+                                                </span>
+                                            @endif
+                                        </div>
+                                    @elseif($comentario->tipo !== 'REVISION')
+                                        <div class="flex items-center justify-between">
+                                            @if($comentario->corregido)
+                                                <span class="inline-flex items-center px-2.5 py-1 bg-green-100 dark:bg-green-900/30 text-green-800 dark:text-green-300 text-xs font-semibold rounded-md">
+                                                    <svg class="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20">
+                                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
+                                                    </svg>
+                                                    Corregido
+                                                </span>
+                                            @else
+                                                <span class="inline-flex items-center px-2.5 py-1 bg-orange-100 dark:bg-orange-900/30 text-orange-800 dark:text-orange-300 text-xs font-semibold rounded-md">
+                                                    <svg class="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20">
+                                                        <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"/>
+                                                    </svg>
+                                                    Pendiente
+                                                </span>
+                                            @endif
+                                        </div>
+                                    @endif
                                 </div>
                             @endforeach
                         @endif
