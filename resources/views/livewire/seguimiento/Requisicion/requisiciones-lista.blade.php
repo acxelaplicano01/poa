2b3a8bd9633c958349d505b0e2b74bc580c3bb70
Vanessa Lainez <vanessa.lainez@ingeniacode.com>
2026-02-18 04:17:19 -0600

    ! arreglando vistas

--- resources/views/livewire/seguimiento/Requisicion/requisiciones-lista.blade.php
+++ resources/views/livewire/seguimiento/Requisicion/requisiciones-lista.blade.php
@@ -67,7 +67,7 @@
                         </select>
                     </div>
                 </div>
-                
+                !
                 <div class="flex justify-end w-full sm:w-auto">
                     <x-spinner-button onclick="window.location.href='/requisicion'"
                         class="inline-flex items-center px-4 py-2 rounded-md text-sm font-medium bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
